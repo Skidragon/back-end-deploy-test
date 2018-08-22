957d39ae0d8a1828d4059f6265d996008548f663
Simon <Simon_Freelance@hotmail.com>
2018-08-22 14:17:42 -0400

    Added a new route

--- server.js
+++ server.js
@@ -6,5 +6,8 @@ const server = express();
 server.get('/', () => {
     res.status(200).json('hello');
 });
+server.get('/test', () => {
+    res.status(200).json('test');
+});
 const port = process.env.PORT || 3001;
 server.listen(port, () => console.log(`port ${port} operational`));
